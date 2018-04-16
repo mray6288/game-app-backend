3dcf1b4964b73579f66639fc7a0d8bcb53b396f7
github username <github email address>
2018-04-16 14:21:48 -0400

    updated cors settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416143720) do
+ActiveRecord::Schema.define(version: 20180416175849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20180416143720) do
     t.integer "loser_id"
     t.integer "winning_score"
     t.integer "losing_score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
